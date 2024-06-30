7f2af464eacda8ee4225a8325179f35f058b1e0d
jigolaurito93 <jigolaurito93@yahoo.com>
2024-06-30 11:13:50 -0500

    added useformstate hook for error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,32 @@
 import { db } from "@/db";
 import { redirect } from "next/navigation";
 
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  // // Check user's inputs and make sure they're valid
+  // const title = formData.get("title") as string;
+  // const code = formData.get("code") as string;
+
+  // // Create a new record in the database
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+
+  // console.log(snippet);
+
+  // // Redirect the user to the root route
+  // redirect("/");
+
+  return {
+    message: "Title must be longer",
+  };
+}
+
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
     where: { id },
