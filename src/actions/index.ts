0bc6aa3fb7571cf006cc8cd4efccb8d4ba8e9764
jigolaurito93 <jigolaurito93@yahoo.com>
2024-07-02 16:56:18 -0500

    displaying error

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,29 +7,37 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check user's inputs and make sure they're valid
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 3) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return { message: "An error occurred" };
+    }
   }
-
-  if (typeof code !== "string" || code.length < 3) {
-    return {
-      message: "Code must be longer",
-    };
-  }
-
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
 
   // Redirect the user to the root route
   redirect("/");
